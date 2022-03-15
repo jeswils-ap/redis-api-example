818e200f463118bd0b3ec04f9960c8bdfae71d2f
jeswils-ap <jeswils@gmail.com>
2022-03-15 11:42:56 +0800

    Migrate certificate details to .env file

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import uvicorn
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+_KEY_ = os.environ.get('CA_KEY')
+_CERT_ = os.environ.get('CA_CERT')
+
 
 if __name__ == '__main__':
 	uvicorn.run(
 		'queue_test:app', port=443, host='127.0.0.1',
-		ssl_keyfile='/Users/jessewilson/Downloads/PyCharm/certs/domain.key',
-		ssl_certfile='/Users/jessewilson/Downloads/PyCharm/certs/domain.crt'
+		ssl_keyfile=_KEY_,
+		ssl_certfile=_CERT_
 	)
