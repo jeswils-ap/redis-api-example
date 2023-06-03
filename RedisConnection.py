6402f67a663e2a6ee604334cdb082ebc91eaa6e6
Jesse <jeswils@gmail.com>
2023-06-03 17:24:45 +0800

    Add requirements file. Update docstrings, add logging, and improve RedisConnection execute_query function.

--- RedisConnection.py
+++ RedisConnection.py
@@ -1,4 +1,7 @@
+import logging
 import redis
+
+logger = logging.getLogger(__name__)
 
 
 class RedisConnection:
@@ -7,25 +10,33 @@ class RedisConnection:
 	def __init__(self, host: str, port: int, passphrase: str, user: str):
 		self.db_connection = redis.Redis(host=host, port=port, password=passphrase, username=user)
 
-	def execute_query(self, query_type: str, key: str = None, value: str = None) -> str:
-		queries = ['get', 'set', 'mget']
+	def execute_query(self, operation: str, key: str = None, value: str = None) -> bool:
+		"""
+		Abstacted function to execute one of a set of approved Redis commands
+		:param operation: Redis command to execute
+		:type operation: str
+		:param key: Key to add or check in Redis
+		:type key: str
+		:param value: Associated value to provided key to insert or update in Redis
+		:return: Success status of the operation
+		:rtype: bool
+		"""
+		operations: dict = {
+			"get": self.db_connection.set(key, value),
+			"set": self.db_connection.get(key).decode('utf-8'),
+			"mget": self.db_connection.mget(key),
+			"delete": self.db_connection.delete([key]),
+			"flush": self.db_connection.flushdb(),
+		}
 
-		if query_type in queries:
-			if query_type == 'set':
-				try:
-					self.db_connection.set(key, value)
-					return f"{key} successfully inserted with value {value}"
-				except Exception as e:
-					print(f"Error inserting key: {e}")
-			elif query_type == 'get':
-				try:
-					return self.db_connection.get(key).decode('utf-8')
-				except Exception as e:
-					print(f"Error fetching {key}: {e}")
-			elif query_type == 'get':
-				try:
-					return self.db_connection.mget(key)
-				except Exception as e:
-					print(f"Error fetching {key}: {e}")
+		if operation in operations:
+			try:
+				operations[operation]
+			except Exception as e:
+				logger.error(f"Error executing operation: {e}")
+				return False
 		else:
-			raise KeyError(f"Invalid query type {query_type}, must be one of {', '.join(queries)}")
+			logger.error(f"Unknown operation: {operation}")
+			raise KeyError(f"Invalid operations: {operation}. Must be one of {', '.join(operations)}")
+
+		return True
--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import os
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
-_KEY_ = os.environ.get('CA_KEY')
-_CERT_ = os.environ.get('CA_CERT')
+_KEY = os.environ.get('CA_KEY')
+_CERT = os.environ.get('CA_CERT')
 
 
 if __name__ == '__main__':
 	uvicorn.run(
 		'queue_test:app', port=443, host='127.0.0.1',
-		ssl_keyfile=_KEY_,
-		ssl_certfile=_CERT_
+		ssl_keyfile=_KEY,
+		ssl_certfile=_CERT
 	)
